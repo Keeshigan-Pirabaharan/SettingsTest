ddfb4fb3164e9e131490388414bcc3edf7658855
Keeshigan <spkeeshi@gmail.com>
2021-12-21 18:08:36 -0500

    Final with Mistakes

--- app/src/main/java/com/example/settingstest/HomeFragment.kt
+++ app/src/main/java/com/example/settingstest/HomeFragment.kt
@@ -1,33 +1,17 @@
 package com.example.settingstest
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceManager
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +21,28 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        fab_settings.setOnClickLister {
+            findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+        }
+        loadSettings()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadSettings(){
+        val sp = PreferenceManager.getDefaultSharedPreferences(context)
+
+        val signature = sp.getString("signature","")
+        val defaultReplyAction = sp.getString("reply","")
+        val sync = sp.getBoolean("sync", true)
+        val notifications = sp.getBoolean("notifications", false)
+        val volume = sp.getInt("volume_notifications", 0)
+
+        tv_signature.text = "Signature: $signature"
+        tv_reply.text = "Default reply: $defaultReplyAction"
+        tv_sync.text = "Sync: $sync"
+        tv_notifications.text = "Disable notifications: $notifications"
+
+        pb_volume.setProgress(volume, true)
     }
 }
